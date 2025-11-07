dd993705ab36192757698e631e06f5b54a01b4f5
mutasim99 <virginmr882@gmail.com>
2025-11-07 11:34:59 +0600

    is palindrome

---
+++ palindrome.js
@@ -0,0 +1,43 @@
+//Problem Statement
+
+//A palindrome is a word, phrase, or sequence of characters that reads the same backward as it does forward.
+
+//Your task is to write a JavaScript function, isPalindrome(str), that takes a string str
+//and returns true if the string is a palindrome, and false otherwise.
+
+//The function must work for complex phrases, not just single words. To do this, your function must:
+//   Be case-insensitive (i.e., 'R' is treated the same as 'r').
+//   Ignore all non-alphanumeric characters (i.e., spaces, punctuation like commas, colons, periods, etc.).
+
+//? Input and Output
+// "A man, a plan, a canal: Panama" -> true
+// "Level" -> true
+// "car" -> false
+
+const isPalindrome = str => {
+    const normalized = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reversed = normalized.split('').reverse().join('');
+
+    return normalized === reversed
+};
+
+// 
+
+
+const isPalindromeTwoPointer = str => {
+    const normalized = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+    let left = 0;
+    let right = normalized.length - 1;
+
+    while(left < right){
+        if (normalized[left] !== normalized[right]) {
+            return false;
+        }
+        left ++;
+        right --
+    }
+    return true;
+}
+
+console.log(isPalindromeTwoPointer('car'));
